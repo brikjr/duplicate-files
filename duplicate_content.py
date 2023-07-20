24c5c7a0113af268fdc6bf28f1c4ecf03fc7cc05
Rishabh Pandey <81532347+brikjr@users.noreply.github.com>
2023-07-20 15:13:38 -0400

    Update duplicate_content.py
    Made some changes on dict of a dict.
    added def main() function

--- duplicate_content.py
+++ duplicate_content.py
@@ -43,44 +43,54 @@ def check_files_in_folder(folder_path, calculate_file_sizes=False):
 
     """
     # Using collections module defaultdict
-    file_hashes = defaultdict(list)
-    file_sizes = defaultdict(list)
+    # Using a single dictionary to store duplicate files based on hash and size
+    duplicate_files = defaultdict(lambda: defaultdict(list))
     
     # Traverse through the folder and its subdirectories
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             file_hash = get_file_hash(file_path) # Calculate the hash of the file
-            file_hashes[file_hash].append(file_path) # Store the file path with its hash
+            # file_hashes[file_hash].append(file_path) # Store the file path with its hash
             
             if calculate_file_sizes:
                 file_size = os.path.getsize(file_path)
                 file_info = (file_path, file_hash)  # Create a tuple with file_path and file_hash
-                file_hashes[file_size].append(file_path)
+                duplicate_files[file_size][file_hash].append(file_info)
                 
+            # Store the file path with its hash in the dictionary
+            duplicate_files[file_hash]['file_paths'].append(file_path)
+        
         for subfolder in dirs:
             subfolder_path = os.path.join(root, subfolder)
             check_files_in_folder(subfolder_path, calculate_file_sizes)  # Recursive call to check files in nested folder
 
 
     # Iterate over the file hashes and their corresponding file paths
-    for file_hash, file_paths in file_hashes.items():
-        if len(file_paths) > 1: # Check if there are multiple file paths with the same hash
-            print(f"Duplicate files with hash {file_hash}")
-            for file_path in file_paths:
-                print(file_path) # Print the file paths of the duplicate files
+    for size, hash_dict in duplicate_files.items():
+        for file_hash, file_info_list in hash_dict.items():
+            if file_hash == 'file_paths':
+                continue
+                
+        if len(file_info_list) > 1: # Check if there are multiple file paths with the same hash
+            print(f"Duplicate files with hash {file_hash} and size {size} bytes:")
+            for file_info in file_info_list:
+                file_path, _ = file_info  # Unpack the tuple, but we only need the file_path
+                print(f"Name: {os.path.basename(file_path)}")
+                print(f"Absolute Path: {file_path}")
             print()
 
-    if calculate_file_sizes:
-        for file_size, file_info_list in file_sizes.items():
-            if len(file_info_list) > 1:
-                print(f"Duplicate files with size {file_size} bytes")
-                for file_info in file_paths:
-                    file_path, file_hash = file_info  # Unpack the tuple
+    if not calculate_file_sizes:
+        for file_hash, file_paths in duplicate_files.items():
+            if file_hash == 'file_paths':
+                continue
+
+            if len(file_paths) > 1:
+                print(f"Duplicate files with hash {file_hash}:")
+                for file_path in file_paths:
                     print(file_path)
                 print()
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='Check for duplicate files in a folder.')
     parser.add_argument('-f', '--folder', required=True, help='Path to the folder')
     parser.add_argument('-s', '--calculate-sizes', action='store_true', help='Calculate duplicate file sizes')
@@ -90,4 +100,8 @@ if __name__ == '__main__':
     calculate_sizes = args.calculate_sizes
 
     check_files_in_folder(folder_path, calculate_sizes)
+
+if __name__ == '__main__':
+    main()
+
 
